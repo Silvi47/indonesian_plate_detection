6fc948b1d64c28b2a8043190e8e3c58529808c6d
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2023-04-05 13:13:09 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import argparse
+from ultralytics import YOLO
+import cv2
+import pandas as pd
+import os
+from ultralytics.yolo.utils.files import increment_path
+from datetime import datetime
+from PIL import Image
+from pathlib import Path
+# from deploy import *
+# from skimage import io
+# import plotly.express as px
+
+now = datetime.now()
+
+def imgwrite(img, file=Path('image.jpg')):
+    file.parent.mkdir(parents=True, exist_ok=True)
+    f = str(increment_path(file).with_suffix('.jpg'))
+    Image.fromarray(img[..., ::-1]).save(f, quality=95, subsampling=0)
+
+model = YOLO("model_best2.pt")
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--file", required=True, help="Path to the file")
+args = vars(ap.parse_args())
+
+results = model.predict(source=args["file"], save=False, show=False, save_crop=False) # show_crop, save_txt
+# Source: cv2.imread('im.jpg')[:,:,::-1], np.zeros((640,1280,3)), torch.zeros(16,3,320,640)
+
+class_list = ["plate"]
+
+for result in results:
+    bbox_idx = result.boxes.xyxy.tolist()
+    img = result.orig_img
+    print("bbox: ", bbox_idx)
+    # filename = 'gambar%s.png' % i
+    # cv2.imwrite(os.path.join("./img_results/",filename),crop)
+    # print("filename: ", filename)
+
+    for bbox in bbox_idx:
+        x3, y3, x4, y4 = bbox
+        cropped_img = img[int(round(y3)):int(round(y4)), int(round(x3)):int(round(x4))]
+        print("xyxy: ",int(round(x3)),int(round(y3)), int(round(x4)),int(round(y4)))
+        # filename = 'gambar%s%s.png' % i
+        # print("filename: ", filename)
+        # imgwrite(cropped_image,"img.jpg")
